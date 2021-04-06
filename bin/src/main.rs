e66e41c7632e3071a4d326727db1fc6c9f40f1e3
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-04-06 15:34:45 +0200

    feat: use lib

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,4 +1,7 @@
-fn main() {
+use raytracer::{Color, io::write_color};
+use std::io;
+
+fn main() -> io::Result<()> {
     // Image
     const image_width: i32 = 256;
     const image_height: i32 = 256;
@@ -6,19 +9,19 @@ fn main() {
     // Render
     println!("P3\n{} {}\n255", image_width, image_height);
 
+    let mut stdout = std::io::stdout();
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining: {} ", j);
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b = 0.25;
+            let color = Color::from_rgb(
+                i as f64 / (image_width - 1) as f64,
+                j as f64 / (image_height - 1) as f64,
+                0.25
+            );
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            println!("{} {} {}", ir, ig, ib);
+            write_color(&mut stdout, color)?;
         }
     }
     eprintln!("\nDone.");
+    Ok(())
 }
